04f251eb36e4567ec05f12873495c3d3bcc8b866
Eman Radwan <emanalaa11499@gmail.com>
2025-05-23 17:25:24 +0300

    edit color palette

--- src/context/FavoritesContext.jsx
+++ src/context/FavoritesContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const FavoritesContext = createContext();
 
 export default function FavoritesContextProvider({ children }) {
-  const [favorites, setFavorites] = useState();
+  const [favorites, setFavorites] = useState([]);
   return (
     <FavoritesContext.Provider value={{ favorites, setFavorites }}>
       {children}
